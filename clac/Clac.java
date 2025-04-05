d9e843e81f4b0ed2980c5c286031e2822cd033c1
Ravindu Dariju <79430607+RDariju@users.noreply.github.com>
2025-04-05 12:19:27 +0530

    Add files via upload

---
+++ clac/Clac.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clac;
+
+/**
+ *
+ * @author User
+ */
+public class Clac {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
